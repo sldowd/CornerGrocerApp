e44b362b10345a8298047b967369869c80dc7c9b
sdowd <sarahlynnedowd@gmail.com>
2024-08-19 20:37:25 -0400

    menu implemented fixme: bug in option 1

--- main.cpp
+++ main.cpp
@@ -4,18 +4,65 @@
 
 using namespace std;
 
+void printAppleLogo();
+
 int main() {
+    //variable to hold user input
+    string userInput = "";
     
    // instantiate map object
     DataMap frequencyMap("itemSales.txt");
 
     cout << "DataMap created. Size: " << frequencyMap.getMap().size() << endl;
 
-    // testing list report function
-    CreateReports::createListReport(frequencyMap);
+    while (userInput != "4") {
+        printAppleLogo();
+        
+        cout << "MENU" << endl;
+        cout << "*** 1. Search for an item" << endl;
+        cout << "*** 2. Print list report" << endl;
+        cout << "*** 3. Print histogram report" << endl;
+        cout << "*** 4. Exit Application" << endl;
+        cout << "Enter your selection:" << endl;
+
+        cin >> userInput;
+
+        cout << "You selected " << userInput << endl;
+
+        if (userInput == "1") {
+            string searchItem;
+            cout << "What item would you like to search for?" << endl;
+            cin >> searchItem;
+            frequencyMap.getValue(searchItem);
+        } 
+        else if (userInput == "2") {
+            CreateReports::createListReport(frequencyMap);
+        } 
+        else if (userInput == "3") {
+            CreateReports::createHistogramReport(frequencyMap);
+        } 
+        else if (userInput == "4") {
+            cout << "Exiting application." << endl;
+            break;
+        } 
+        else {
+            cout << "Invalid selection" << endl;
+        }
+    }
     
-    CreateReports::createHistogramReport(frequencyMap);
 
     return 0;
 
 }
+
+void printAppleLogo() {
+    cout << "     ,--./,-. " << endl;
+    cout << "    / #      \\" << endl;
+    cout << "   |          |" << endl;
+    cout << "    \\        /" << endl;
+    cout << "     `._,._,' " << endl;
+    cout << endl;
+    cout << "  Corner Grocer" << endl;
+    cout << endl;
+}
+
